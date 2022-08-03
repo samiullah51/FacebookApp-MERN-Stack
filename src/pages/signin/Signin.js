cfcae35fe60e9598f6b0f1c7a93287bd91dcac89
samiullah <samii51728@gmail.com>
2022-08-03 11:16:44 -0700

    Signin Page created

---
+++ src/pages/signin/Signin.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Signin.css";
+function Signin() {
+  return (
+    <div className="signin">
+      {/* Left Section of the page */}
+      <div className="left">
+        {/* Facebook logo */}
+        <img
+          src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg"
+          alt="Facebook logo"
+        />
+        <p>
+          Facebook helps you connect and share with the people in your life.
+        </p>
+      </div>
+      {/* Right Section of the page */}
+      <div className="right">
+        {/* Form Container */}
+        <div className="form__container">
+          <input type="text" placeholder="Email address or phone number" />
+          <input type="password" placeholder="password" />
+          <button className="loginBtn">Log In</button>
+          <a href="#">Forgotton Password?</a>
+          <div className="line"></div>
+          <button className="createBtn">Create New Account</button>
+        </div>
+        <p>
+          <b>Create a Page </b> for a celebrity, brand or business.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Signin;
