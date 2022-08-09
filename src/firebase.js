4af5550fc497bd9d3f7505558fbed27a6fe0a080
samiullah <samii51728@gmail.com>
2022-08-09 19:32:00 +0500

    deleted firebase link

--- src/firebase.js
+++
@@ -1,5 +0,0 @@
-const Key =
-  "BLxKaG2lnf-KL_w8yu15I1S5e6WGYuRPlFKpr50BujoqbOzmk9hgcqq-m_LG08lj6Yz26Zl_5CAhjXK-85DYYFY";
-
-//   key
-//BLxKaG2lnf-KL_w8yu15I1S5e6WGYuRPlFKpr50BujoqbOzmk9hgcqq-m_LG08lj6Yz26Zl_5CAhjXK-85DYYFY
