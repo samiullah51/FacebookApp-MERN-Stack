0de944cf0e9efccab9e94bde8ad44fa58254d639
samiullah <samii51728@gmail.com>
2022-08-10 16:59:33 +0500

    start working on backend

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const connection = require("./connection");
+
+// dotenv Configuration
+dotenv.config();
+
+// Database Connection
+
+connection();
+// Listening to a port
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
