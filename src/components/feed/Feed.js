3c18b401a4985be9d3b7e3f4fdffce2c42e94a48
samiullah <samii51728@gmail.com>
2022-08-16 17:56:44 +0500

    modal for new post is created

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -9,10 +9,13 @@ import { useSelector } from "react-redux";
 function Feed() {
   const user = useSelector((state) => state.user);
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     const fetchPosts = async () => {
       const result = await axios.get("http://localhost:8000/api/post/allposts");
       setPosts(result.data);
+      setLoading(false);
     };
     fetchPosts();
   }, [user]);
@@ -25,9 +28,11 @@ function Feed() {
       {/* post form */}
       <Postform />
       {/* Post */}
-      {posts.map((post) => (
-        <Post post={post} />
-      ))}
+      {loading ? (
+        <h3 style={{ textAlign: "center" }}>Loading...</h3>
+      ) : (
+        posts.map((post) => <Post post={post} />)
+      )}
     </div>
   );
 }
--- src/components/feed/postform/Postform.js
+++ src/components/feed/postform/Postform.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Postform.css";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import CollectionsIcon from "@mui/icons-material/Collections";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import { useSelector } from "react-redux";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 function Postform() {
   const user = useSelector((state) => state.user);
+  const [modal, setModal] = useState(false);
+  const [image, setImage] = useState("");
+  const [desc, setDesc] = useState("");
+  // handleNewPost
+  const handleNewPost = () => {
+    setModal(true);
+  };
 
+  // handleCancel
+  const handleCancel = () => {
+    setImage("");
+    setDesc("");
+    setModal(false);
+  };
   return (
     <div className="postfrom">
       <div className="postform__up">
@@ -22,7 +36,7 @@ function Postform() {
           <VideocamIcon className="icon" style={{ color: "red" }} />
           <p>Live video</p>
         </div>
-        <div className="icon__group">
+        <div className="icon__group" onClick={handleNewPost}>
           <CollectionsIcon className="icon" style={{ color: "green" }} />
           <p>Photo/video</p>
         </div>
@@ -31,6 +45,50 @@ function Postform() {
           <p>Feeling/activity</p>
         </div>
       </div>
+      {/* modal */}
+      {modal && (
+        <div className="modal">
+          <div className="modal__box">
+            {/* Modal header */}
+            <div className="modal__header">
+              <h3>Create New Post</h3>
+              <p onClick={() => setModal(false)}>&times;</p>
+            </div>
+            <div className="line"></div>
+            {/* body */}
+            <div className="modal__body">
+              <textarea
+                rows={4}
+                type="text"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+                placeholder="Write something about your post..."
+              />
+              {image && <img src={image} />}
+
+              <label for="inputTag">
+                <CameraAltIcon className="icon" />
+                <input
+                  id="inputTag"
+                  type="file"
+                  onChange={(e) =>
+                    setImage(URL.createObjectURL(e.target.files[0]))
+                  }
+                  style={{ display: "none" }}
+                />
+              </label>
+            </div>
+            {/* Footer */}
+            <div className="line"></div>
+            <div className="modal__footer">
+              <button className="cancelBtn" onClick={handleCancel}>
+                Cancel
+              </button>
+              <button className="postBtn">Post</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
