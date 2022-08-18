4e43b616973160a7c391bcec8a669f1441a6a0a0
samiullah <samii51728@gmail.com>
2022-08-18 15:40:17 +0500

    single post added

--- src/pages/profile/singlepost/SinglePost.js
+++ src/pages/profile/singlepost/SinglePost.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./SinglePost.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 function SinglePost({ singlePost }) {
   return (
     <div className="single__post">
       <img src={singlePost.poster} />
+      <div className="icon">
+        <DeleteIcon />
+      </div>
     </div>
   );
 }
